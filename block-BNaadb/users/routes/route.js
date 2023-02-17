26e683db5b1d0f0514f4dc0b7599db6bd6a62717
ajay <ajayrajput9554@gmail.com>
2023-02-17 22:41:20 +0530

    delete operation express-crud

---
+++ block-BNaadb/users/routes/route.js
@@ -0,0 +1,57 @@
+var express=require('express')
+var router=express.Router()
+var users=require('../model/user')
+
+router.get('/new',(req,res)=>{
+    res.render('adduser')
+})
+
+router.post('/',(req,res)=>{
+    console.log(req.body)
+    users.create(req.body,(err,createdform)=>{
+        if(err){
+            res.redirect('/users/new')
+        }
+        res.redirect('/users')
+        console.log(createdform)
+    })
+})
+
+router.get('/',(req,res)=>{
+    users.find({},req.body,(err,users)=>{
+        if(err) return next()
+        res.render('users',{users:users})
+    })
+})
+
+router.get('/:id',(req,res,next)=>{
+    var id=req.params.id
+    users.findById(id ,(err,user)=>{
+        if(err)return  next()
+        res.render('singleuser',{user:user})
+    })
+})
+
+router.get('/:id/edit',(req,res,next)=>{
+    users.findById(req.params.id,(err,user)=>{
+        if(err) return next()
+        res.render('updateuser.ejs',{user:user})
+    })
+})
+
+router.post('/:id',(req,res,next)=>{
+    users.findByIdAndUpdate(req.params.id,req.body,{new: true},(err,user)=>{
+        if(err)return next()
+        // res.render('singleuser',{user})
+        res.redirect('/users')
+    })
+})
+
+router.get('/:id/delete',(req,res,next)=>{
+    users.findByIdAndDelete(req.params.id,(err,deleteuser)=>{
+        if(err) return next()
+        res.redirect('/users')
+    })
+})
+
+module.exports=router
